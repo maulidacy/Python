6c8020d13911eae607d7dbb84bb0b68cbaf5dc3d
maulidacy <cahyamaulida011@gmail.com>
2025-02-01 14:34:43 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+# Operasi Aritmatika Sederhana
+print(28*"=")
+print("Operasi Aritmatika Sederhana")
+print(28*"=" + "\n")
+
+angka_1 = float(input("Masukkan angka pertama: "))
+angka_2 = float(input("Masukkan angka kedua: "))
+print(f"Penjumlahan: ", angka_1 + angka_2)
+print(f"Pengurangan: ", angka_1 - angka_2)
+print(f"Perkalian: ", angka_1 * angka_2)
+print(f"Pembagian: ", angka_1 / angka_2)
+
+exit()
+
+
+
+# Hitung Luas Lingkaran
+print(20*"=")
+print("Hitung Luas Lingkaran")
+print(20*"=" + "\n")
+
+r = float(input("Masukkan jari-jari lingkaran : "))
+luas = 3.14 * (r * r)
+
+print(f"Luas lingkaran adalah: {luas}")
+exit()
+
+
+
 # Deklarasi Variabel
-print("-----Deklarasi Variabel-----")
+print(20*"=")
+print("Deklarasi Variabel")
+print(20*"=" + "\n")
 
 angka_bulat = int(input("Angka bulat: "))
 print(type(angka_bulat))
