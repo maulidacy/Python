f596471509c43068863b61a97b803366da43b8c4
maulidacy <cahyamaulida011@gmail.com>
2025-02-01 14:13:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Deklarasi Variabel
+print("-----Deklarasi Variabel-----")
+
+angka_bulat = int(input("Angka bulat: "))
+print(type(angka_bulat))
+
+angka_pecahan = float(input("Angka pecahan: "))
+print(type(angka_pecahan))
+
+teks = input("Teks: ")
+print(type(teks))
+
+boolean = bool(input("True/False: "))
+print(type(boolean))
+
+input_list = list(input("List: "))
+print(type(input_list))
+
+exit()
