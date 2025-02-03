0499ac2fbbe0775beeeb516752f96429f0c6fc0f
maulidacy <cahyamaulida011@gmail.com>
2025-02-03 15:03:28 +0700

    update

--- main2.py
+++ main2.py
@@ -1,4 +1,37 @@
-#Konversi Suhu
+# Menghitung Rata-rata Nilai
+print(25*"=")
+print("Menghitung Rata-rata Nilai")
+print(25*"=" + "\n")
+
+n = int(input("Masukkan jumlah mata pelajaran: "))
+total_nilai = 0
+
+# Looping untuk meminta input nilai setiap mata pelajaran
+for i in range(n):
+    nilai = float(input(f"Masukkan nilai ke-{i + 1}: "))
+    total_nilai += nilai
+    rata_rata = total_nilai / n
+print(f"Rata-rata nilai: {rata_rata}")
+
+exit()
+
+
+# Kalkulator Diskon
+print(20*"=")
+print("Kalkulator Diskon")
+print(20*"=" + "\n")
+
+harga_barang = float(input("Masukkan harga barang: "))
+diskon = float(input("Masukkan persentase diskon: "))
+
+jumlah_diskon = (diskon / 100) * harga_barang
+harga_setelah_diskon = harga_barang - jumlah_diskon
+
+print(f"Harga setelah diskon: {harga_setelah_diskon}")
+exit()
+
+
+# Konversi Suhu
 print(20*"=")
 print("Konversi Suhu")
 print(20*"=" + "\n")
