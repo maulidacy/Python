3c42fb07f864a14624840a04cf28f4eae51c5620
maulidacy <cahyamaulida011@gmail.com>
2025-02-18 21:14:19 +0700

    update

--- continue_pass.py
+++ continue_pass.py
@@ -1,5 +1,26 @@
+#------------------------Melewati Huruf Vokal dalam Kata------------------------
+huruf_vokal = "aiueoAIUEO"
+kata = input("Masukkan kata: ")
+hasil = ""
+
+for huruf in kata:
+    if huruf in huruf_vokal:
+        continue
+    hasil += huruf # Menambahkan huruf ke hasil jika bukan vokal
+print("Kata tanpa huruf vokal: ", hasil)
+exit()
+
+
+#------------------------Melewati Bilangan Genap------------------------
+for angka in range(1, 11):
+    if angka % 2 == 0:
+        continue #akan melewati angka genap
+    print("Angka: ", angka)
+exit()
+
+
 #------------------------Melewati Angka Tertentu------------------------
 for angka in range(1, 11):
     if angka == 5:
-        continue
+        continue #akan melewati angka 5
     print("Angka: ", angka)
