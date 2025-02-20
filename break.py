ec5988fbdf6bcd0ccfea4ce957a52bc1c2324870
maulidacy <cahyamaulida011@gmail.com>
2025-02-20 14:39:36 +0700

    update

--- break.py
+++ break.py
@@ -1,3 +1,19 @@
+#------------------------Menebak Angka------------------------.
+import random
+angka = random.randint(1, 20) # Komputer memilih angka acak dari 1 hingga 20
+tebakan = None # Inisialisasi variabel untuk menyimpan tebakan
+
+while tebakan != angka:
+    tebakan = int(input("Tebakan Anda: "))
+    if tebakan == angka:
+        print("Selamat! Anda benar")
+        break
+    else:
+        print("Maaf, Anda salah. Coba lagi!")
+exit()
+
+
+
 #------------------------Mencari Angka Tertentu------------------------
 i = [2, 4, 6, 8, 10, 15, 20]
 
