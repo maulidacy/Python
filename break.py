3011ca5c35f95756e1c295ccf36dc9d2fd745ff0
maulidacy <cahyamaulida011@gmail.com>
2025-02-20 14:18:21 +0700

    update

---
+++ break.py
@@ -0,0 +1,7 @@
+#------------------------Mencari Angka Tertentu------------------------
+i = [2, 4, 6, 8, 10, 15, 20]
+
+for angka in i:
+    if angka > 10:
+        print("Angka pertama yang lebih dari 10 adalah:", angka)
+        break
