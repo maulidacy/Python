493b5663cd8597187f90fe6e5b10f08f5caa10de
maulidacy <cahyamaulida011@gmail.com>
2025-02-23 20:18:17 +0700

    update

---
+++ List.py
@@ -0,0 +1,7 @@
+#------------------------Buat dan Akses List------------------------
+buah = ["apel", "durian", "pisang", "semangka", "jeruk"]
+print(buah[0], buah[-3], buah[-1])
+
+print(buah[0])
+print(buah[-3])
+print(buah[-1])
