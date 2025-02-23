7abdb5c13976c1a8d45649b0202464908693b977
maulidacy <cahyamaulida011@gmail.com>
2025-02-23 20:22:28 +0700

    update

--- List.py
+++ List.py
@@ -1,3 +1,12 @@
+#------------------------Mengubah Elemen dalam List------------------------
+warna = ["merah", "biru", "hijau", "kuning"]
+warna[2] = "ungu"
+warna[0] = "hitam"
+
+print(warna)
+exit()
+
+
 #------------------------Buat dan Akses List------------------------
 buah = ["apel", "durian", "pisang", "semangka", "jeruk"]
 print(buah[0], buah[-3], buah[-1])
