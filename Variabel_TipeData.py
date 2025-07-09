e46ac9a2219a6f0564efd81141cd1654d3274007
maulidacy <cahyamaulida011@gmail.com>
2025-07-09 07:19:53 +0700

    first

---
+++ Variabel_TipeData.py
@@ -0,0 +1,14 @@
+#------------------------- 1. Buat 3 variabel: nama (string), umur (integer), tinggi (float) -------------------------
+nama = "Budi"
+umur = 0
+tinggi = 1.75
+#------------------------- 2. Cetak kalimat: "Halo, nama saya ___, umur saya ___ tahun." -------------------------
+print(f"Nama: {nama}, Umur: {umur} tahun, Tinggi: {tinggi} meter")
+
+#------------------------- 3. Hitung dan tampilkan hasil dari: 5 + 2 * 10 - 3 / 2 -------------------------
+a = 5
+b = 2
+c = 10
+d = 3
+Hitung = a+b*c-d/b
+print(f"Hasil perhitungan: {Hitung}")
