9fa3bb2cd7f75458ebb7a17f54a00183823b65e5
maulidacy <cahyamaulida011@gmail.com>
2025-07-10 14:17:49 +0700

    first commit

---
+++ If-elif-else.py
@@ -0,0 +1,36 @@
+
+#------------------------- 3. Menghitung Nilai Akhir Mahasiswa ------------------------- 
+nilai_uts = float(input("Nilai UTS: ")) 
+nilai_uas = float(input("Nilai UAS: ")) 
+nilai_tugas = float(input("Nilai Tugas: ")) 
+Hnilai_uts = nilai_uts * 0.3
+Hnilai_uas = nilai_uas * 0.3
+Hnilai_tugas = nilai_tugas * 0.4
+total_nilai = Hnilai_uts + Hnilai_uas + Hnilai_tugas
+
+if total_nilai >= 85: 
+    print("Nilai Akhir = A")
+elif total_nilai >= 70: 
+    print("Nilai Akhir = B")
+elif total_nilai >= 60: 
+    print("Nilai Akhir = C")
+else: 
+    print("Nilai Akhir = D")
+exit()
+#------------------------- 2. Cek Bilangan Ganjil atau Genap -------------------------
+bilangan = int(input("Masukan bilangan: "))
+if bilangan % 2 == 0: 
+    print("Bilangan genap")
+elif bilangan % 2 != 0:
+    print("Bilangan ganjil")
+exit()
+
+#------------------------- 1. Cek Angka Positif atau Negatif -------------------------
+angka = int(input("Masukkan angka: "))
+
+if angka > 0:
+    print("Angka positif")
+elif angka < 0:
+    print("Angka negatif")
+elif angka == 0:
+    print("Angka nol")
