92a850b6a2e650bef3812e3dad6d2725dca0f6b6
maulidacy <cahyamaulida011@gmail.com>
2025-07-14 12:58:01 +0700

    first commit

--- If-elif-else.py
+++ If-elif-else.py
@@ -1,3 +1,24 @@
+nilai_uts = float(input("Nilai UTS: ")) 
+nilai_uas = float(input("Nilai UAS: ")) 
+nilai_tugas = float(input("Nilai Tugas: ")) 
+Hnilai_uts = nilai_uts * 0.3
+Hnilai_uas = nilai_uas * 0.3
+Hnilai_tugas = nilai_tugas * 0.4
+total_nilai = Hnilai_uts + Hnilai_uas + Hnilai_tugas
+
+if total_nilai >= 85: 
+    print("Nilai Akhir = A")
+    print("Selamat, Anda lulus dengan nilai A!")
+elif total_nilai >= 70: 
+    print("Nilai Akhir = B")
+    print("Bagus, Anda lulus dengan nilai B!")
+elif total_nilai >= 60: 
+    print("Nilai Akhir = C")
+    print("Anda lulus dengan nilai C, perbaiki di ujian berikutnya.")
+else: 
+    print("Nilai Akhir = D")
+    print("Maaf, Anda tidak lulus. Silakan coba lagi di semester berikutnya.")
+exit()
 
 #------------------------- 4. Login Sederhana -------------------------
 user = input("username = ") 
