1955c68dc798b33c5208e7675a1b570c9324aa7b
maulidacy <cahyamaulida011@gmail.com>
2025-07-21 13:09:12 +0700

    first commit

---
+++ Function.py
@@ -0,0 +1,12 @@
+def ucapan(): print("Selamat belajar Python!")
+ucapan() 
+
+def luas_persegi(sisi): 
+    hasil = sisi * sisi 
+    print(f"Luas persegi adalah: {hasil}") 
+luas_persegi(5) 
+    
+#------------------------- Parameter ganda -------------------------
+def salam (nama, waktu): 
+    print(f"Selamat {waktu}, {nama}!")
+salam("Maulida", "pagi")
