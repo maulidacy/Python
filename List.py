66044a0d5d43da328a82b5b4bbff496e433b2ee5
maulidacy <cahyamaulida011@gmail.com>
2025-07-28 21:14:34 +0700

    first commit

--- List.py
+++ List.py
@@ -1,3 +1,11 @@
+#------------------------Menampilkan Bilangan Genap dalam List------------------------
+bilangan = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for i in bilangan:
+    if i % 2 == 0:
+        print(f"{i}", end=" ")
+exit()
+
 #------------------------Menyusun List dari Input Pengguna------------------------
 stok = ["sandal", "sepatu", "tas", "topi"]
 
