eb00c678f4aa58d90bb9769344cef8fc510b49af
maulidacy <cahyamaulida011@gmail.com>
2025-08-01 19:58:41 +0700

    first commit

---
+++ List_comprehension.py
@@ -0,0 +1,8 @@
+#------------------------------Kuadrat List-----------------------------
+def kuadrat_list(data):
+    return [i**2 for i in data]
+
+# Contoh penggunaan:
+data = [1, 2, 3, 4, 5]
+print(kuadrat_list(data))
+#----------------------------------------------------------------------
