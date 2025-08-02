84576f0d6ab0b2037ea0ef94086fb7c2a51046e0
maulidacy <cahyamaulida011@gmail.com>
2025-08-02 19:48:58 +0700

    first commit

--- List_comprehension.py
+++ List_comprehension.py
@@ -1,3 +1,15 @@
+#-------------------------------Ganda Tiga atau Ganjil-------------------------------
+angka = list(range(1, 21))
+hasil = []
+
+for i in angka:
+    if i % 3 == 0:
+        hasil.append("Fizz")
+    elif i % 2 != 0:
+        hasil.append("Ganjil")
+print(hasil)
+exit()
+
 #------------------------------Kuadrat List-----------------------------
 def kuadrat_list(data):
     return [i**2 for i in data]
