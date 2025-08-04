0ea97b32e077081eef06406286e4622cb19b4964
maulidacy <cahyamaulida011@gmail.com>
2025-08-04 20:02:13 +0700

    first commit

--- try-except.py
+++ try-except.py
@@ -1,5 +1,15 @@
 #-----------------------------------------------------------------
 try:
+    pembilang = int(input("Masukkan pembilang: "))
+    penyebut = int(input("Masukkan penyebut: "))
+    hasil = pembilang / penyebut
+    print(f"Hasil pembagian: {hasil}")
+except ZeroDivisionError:
+    print("Penyebut tidak boleh nol!")
+exit()
+
+#-----------------------------------------------------------------
+try:
     angka = int(input("Masukkan angka: "))
 except ValueError:
     print("Input harus berupa angka!")
