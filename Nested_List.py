085670bbfe577eb93951b515c033700582ecb76e
maulidacy <cahyamaulida011@gmail.com>
2025-08-09 12:59:27 +0700

    first commit

--- Nested_List.py
+++ Nested_List.py
@@ -9,4 +9,16 @@ total = 0
 for i in range(len(produk)):
     total += produk[i][1] * produk[i][2]
 print(f"Total pendapatan: {total}")
+
+#versi comprehension
+total = sum([harga * jumlah for _, harga, jumlah in produk])
+# for _, harga, jumlah in produk → langsung unpack elemen nested list.
+# _ → nama produk (pakai _ artinya abaikan nilainya).
+# harga * jumlah → hitung pendapatan per produk.
+# sum([...]) → jumlahkan semua pendapatan.
+print(f"Total pendapatan: {total}")
+
+# versi lebih clean lagi tanpa tanda [] (pakai generator expression)
+total = sum(harga * jumlah for _, harga, jumlah in produk)
+print(f"Total pendapatan: {total}")
 #-------------------------------------------------------------------------------------------------------
