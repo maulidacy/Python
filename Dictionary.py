5b4db708e2b4cdbea0f4216939fc55e0b9ba7058
maulidacy <cahyamaulida011@gmail.com>
2025-08-22 13:32:49 +0700

    update

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,31 @@
+# "key": "value" adalah format dari dictionary
+# Dictionary adalah tipe data yang menyimpan data dalam bentuk pasangan key-value.  
+# Key bersifat unik, artinya tidak boleh ada key yang sama dalam satu dictionary.
+# Nilai dari key bisa berupa tipe data apa saja, termasuk string, integer, list, atau bahkan dictionary itu sendiri.
+# Untuk mengakses nilai, kita menggunakan key yang sesuai.
+
+# ----------------------------------------------------------------------------------------------------------------
+mahasiswa = {
+    "nama": "Maul",
+    "umur": 20,
+    "jurusan": "Teknik Informatika"
+}
+
+#cetak hanya nama
+print(mahasiswa["nama"])
+
+#tambah data baru
+mahasiswa["angkatan"] = 2023
+print(mahasiswa)
+
+#ubah data
+mahasiswa["umur"] = 21
+
+#hapus data
+del mahasiswa["jurusan"]
+print(mahasiswa)
+exit()
+
 #-------------------------------------------------- membuat dict --------------------------------------------------
 produk = {
     "nama": "Roti", 
